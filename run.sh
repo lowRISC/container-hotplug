10bdf4153fc491ea0148c9ad33ef973dfcfd42b8
Jorge Prendes <jorge.prendes@gmail.com>
2022-10-06 17:20:53 +0100

    Add support for serial number in run.sh script

--- run.sh
+++ run.sh
@@ -2,10 +2,15 @@
 
 comment=
 
+DEVICE="usb:2b3e:c310"
+if [ "$1" != "" ]; then
+    DEVICE="$DEVICE:$1"
+fi
+
 # shellcheck disable=SC2086
 sudo target/debug/container-ci-hotplug \
     run \
-    -d parent-of:usb:2b3e:c310 \
+    -d parent-of:${DEVICE} \
     ${comment# NewAE ChipWhisperer CW310 } \
     -l usb:2b3e:c310:1=/dev/ttyACM_CW310_0 \
     -l usb:2b3e:c310:3=/dev/ttyACM_CW310_1 \
@@ -17,7 +22,7 @@ sudo target/debug/container-ci-hotplug \
     -- \
     ${comment# Docker arguments } \
     -it \
-    --name hotplug-cw310 \
+    --name hotplug-"$(echo "$DEVICE" | tr : _)" \
     -v "$PWD"/ott:/ott \
     ubuntu:22.04 \
     /ott/start.sh

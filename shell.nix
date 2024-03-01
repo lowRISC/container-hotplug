7145210ae21f94536a57d671d4917d5508c845c4
Gary Guo <gary.guo@lowrisc.org>
2024-03-01 12:41:19 +0000

    Add updated bpf linker into nix env

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   buildInputs = with pkgs; [udev];
-  nativeBuildInputs = with pkgs; [rustup pkg-config];
+  nativeBuildInputs = with pkgs; [
+    rustup
+    pkg-config
+    (pkgs.callPackage deps/bpf-linker.nix { useRustLlvm = true; })
+
+    # For llvm-objdump
+    llvmPackages.bintools
+  ];
 }

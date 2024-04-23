65bcd9100731b125687d30dcee0934dc8089e2db
Gary Guo <gary.guo@lowrisc.org>
2024-04-23 16:01:53 +0100

    Add syslog logger

--- src/util/log.rs
+++ src/util/log.rs
@@ -1,6 +1,9 @@
+use std::os::unix::net::UnixDatagram;
 use std::sync::{OnceLock, RwLock};
 
-use log::Log;
+use anyhow::{Context, Result};
+use log::{Level, Log};
+use rustix::thread::Pid;
 
 pub struct ReplaceableLogger {
     logger: RwLock<Box<dyn Log>>,
@@ -43,4 +46,53 @@ pub fn global_replace(logger: Box<dyn Log>) {
         this.replace(logger);
     }
     let _ = log::set_logger(this);
+}
+
+pub struct SyslogLogger {
+    socket: UnixDatagram,
+    exe: String,
+    pid: Pid,
+}
+
+impl SyslogLogger {
+    pub fn new() -> Result<Self> {
+        let socket = UnixDatagram::unbound()?;
+        socket.connect("/dev/log")?;
+        let exe = std::env::current_exe()?
+            .file_name()
+            .context("cannot get process name")?
+            .to_str()
+            .context("process name is not UTF-8")?
+            .to_owned();
+        let pid = rustix::process::getpid();
+        Ok(Self { socket, exe, pid })
+    }
+}
+
+impl Log for SyslogLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        let severity = match record.level() {
+            Level::Error => 3,
+            Level::Warn => 4,
+            Level::Info => 5,
+            Level::Debug => 6,
+            Level::Trace => 7,
+        };
+        const FACILITY: u8 = 1; // indicates user-level messages.
+        let priority = FACILITY << 3 | severity;
+
+        let msg = format!(
+            "<{priority}>{exe}[{pid}]: {msg}",
+            exe = self.exe,
+            pid = self.pid.as_raw_nonzero(),
+            msg = record.args()
+        );
+        let _ = self.socket.send(msg.as_bytes());
+    }
+
+    fn flush(&self) {}
 }

1ab4ef0dbdad8d8fa3db8f63fdc219b92c499072
Gary Guo <gary.guo@lowrisc.org>
2025-03-18 13:41:57 +0000

    Use flake instead shell to allow using nixpkgs-unstable
    BPF targets currently require nightly, and nixpkgs-unstable have
    (unreleased) LLVM 20 which is needed to work with latest nightly.

---
+++ flake.nix
@@ -0,0 +1,38 @@
+# Copyright lowRISC Contributors.
+# Licensed under the MIT License, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [udev];
+            nativeBuildInputs = with pkgs; [
+              rustup
+              pkg-config
+              bpf-linker
+
+              # For llvm-objdump
+              llvmPackages_20.bintools
+
+              # To aid testing
+              runc
+            ];
+          };
+        };
+        formatter = pkgs.alejandra;
+      }
+    );
+}
